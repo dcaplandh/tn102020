469f3e2b554bf193d2321cfaf9dcc714a7fdda7a
diego <dcaplan@digitalhouse.com>
2020-04-01 20:14:25 -0300

    clase 2 html css repaso

--- frontend/routes/ejercicios.js
+++ frontend/routes/ejercicios.js
@@ -3,11 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/clase1/ejemplo1', function(req, res, next) {
-  res.render('ejemplo1');
+  res.render('clase1/ejemplo1');
 });
 router.get('/clase1/ejemplo2', function(req, res, next) {
-    res.render('ejemplo2');
+    res.render('clase1/ejemplo2');
 });
-
+router.get('/clase2/ejemplo1', function(req, res, next) {
+  res.render('clase2/ejemplo1');
+});
+router.get('/clase2/ejemplo2', function(req, res, next) {
+  res.render('clase2/ejemplo2');
+});
 
 module.exports = router;

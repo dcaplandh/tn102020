6219d32fd692c8845ed45f92da21d3e9e443a1d0
diego <dcaplan@digitalhouse.com>
2020-04-15 21:06:36 -0300

    flexbox y bootstrap

--- frontend/routes/ejercicios.js
+++ frontend/routes/ejercicios.js
@@ -14,5 +14,20 @@ router.get('/clase2/ejemplo1', function(req, res, next) {
 router.get('/clase2/ejemplo2', function(req, res, next) {
   res.render('clase2/ejemplo2');
 });
+router.get('/clasevirtual/maqueta', function(req, res, next) {
+  res.render('clasevirtual/maqueta');
+});
+router.get('/clase3/ejemplo1', function(req, res, next) {
+  res.render('clase3/ejemplo1');
+});
+router.get('/clase3/ejemplo2', function(req, res, next) {
+  res.render('clase3/ejemplo2');
+});
+router.get('/clase4/flexbox', function(req, res, next) {
+  res.render('clase4/flexbox');
+});
+router.get('/clase4/bootstrap', function(req, res, next) {
+  res.render('clase4/bootstrap');
+});
 
 module.exports = router;
